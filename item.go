3475af9c3218e8f02090bcc709198677aaa38214
0x0001a2040 <robert_0.1@gmx.de>
2020-08-05 01:46:27 +0200

    Add item.go

---
+++ item.go
@@ -0,0 +1,13 @@
+package gw2api
+
+// ItemStack is the data contained in requests that return stored items, e.g. bank or shared inventory
+type ItemSlot struct {
+	ID        string `json:"id"`
+	Count     int    `json:"count"`
+	Charges   int    `json:"charges"`
+	Skin      int    `json:"skin"`
+	Upgrades  []int  `json:"upgrades"`
+	Infusions []int  `json:"infusions"`
+	Binding   string `json:"binding"`
+	BoundTo   string `json:"bound_to"`
+}
