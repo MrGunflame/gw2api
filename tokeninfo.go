dffa1746755d25a7e8fd2fa7212bd5c68ca0f418
0x0001a2040 <robert_0.1@gmx.de>
2020-08-05 00:24:56 +0200

    Add comments

--- tokeninfo.go
+++ tokeninfo.go
@@ -1,11 +1,13 @@
 package gw2api
 
+// TokenInfo contains general token information, such as the permissons
 type TokenInfo struct {
   ID string `json:"id"`
   Name string `json:"name"`
   Permissions []string `json:"permissions"`
 }
 
+// GetTokenInfo returns general information about the provided token
 func (s *Session) GetTokenInfo() (tokeninfo TokenInfo, err error) {
   err = s.get("/v2/tokeninfo", &tokeninfo)
   return
