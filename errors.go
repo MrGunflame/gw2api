f73a4a5ab0b6b0f5ff5eb2494a41d70b02e5c0f6
0x0001a2040 <robert_0.1@gmx.de>
2020-08-05 01:24:26 +0200

    Update errors.go

--- errors.go
+++ errors.go
@@ -4,22 +4,23 @@ import (
 	"errors"
 )
 
+// Error constants
 var (
 	ErrNotFound           = errors.New("not found")
 	ErrInvalidAccessToken = errors.New("Invalid access token")
 )
 
-type Error struct {
-	HTTPError string `json:"error"`
-	Text      string `json:"text"`
-}
+// type Error struct {
+// 	HTTPError string `json:"error"`
+// 	Text      string `json:"text"`
+// }
 
-func (e *Error) Err() error {
-	switch {
-	case e.HTTPError == "not found":
-		return ErrNotFound
-	case e.Text == "Invalid access token":
-		return ErrInvalidAccessToken
-	}
-	return nil
-}
+// func (e *Error) Err() error {
+// 	switch {
+// 	case e.HTTPError == "not found":
+// 		return ErrNotFound
+// 	case e.Text == "Invalid access token":
+// 		return ErrInvalidAccessToken
+// 	}
+// 	return nil
+// }
