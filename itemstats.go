e1ae2e3a19459e1798ec8d186938cf7bdf624d4c
0x0001a2040 <robert_0.1@gmx.de>
2020-08-06 00:10:01 +0200

    Update itemstats.go

--- itemstats.go
+++ itemstats.go
@@ -1,5 +1,6 @@
 package gw2api
 
+// Itemstat contains the stattype and attributes of an item
 type Itemstat struct {
 	ID         int            `json:"id"`
 	Attributes map[string]int `json:"attributes"`
