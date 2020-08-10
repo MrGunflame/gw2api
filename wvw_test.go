80221a6c659b78bba876af44cdeb85b6bca01dbb
0xc0001a2040 <robert_0.1@gmx.de>
2020-08-10 02:49:22 +0200

    Add wvw_test.go

---
+++ wvw_test.go
@@ -0,0 +1,27 @@
+package gw2api
+
+import "testing"
+
+func TestWvW(t *testing.T) {
+	api := New()
+
+	if _, err := api.GetWvWAbilities(); err != nil {
+		t.Error("GetWvWAbilities failed: ", err)
+	}
+
+	if _, err := api.GetWvWMatches(); err != nil {
+		t.Error("GetWvWMatches failed: ", err)
+	}
+
+	if _, err := api.GetWvWObjectives(); err != nil {
+		t.Error("GetWvWObjectives failed: ", err)
+	}
+
+	if _, err := api.GetWvWRanks(); err != nil {
+		t.Error("GetWvWRanks failed: ", err)
+	}
+
+	if _, err := api.GetWvWUpgrades(); err != nil {
+		t.Error("GetWvWUpgrades failed: ", err)
+	}
+}
