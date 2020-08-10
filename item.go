d9e3a8cc23f0fa052950e0f2f1c0fd5c8426ab0e
0xc0001a2040 <robert_0.1@gmx.de>
2020-08-10 19:05:37 +0200

    Add Item

--- item.go
+++ item.go
@@ -1,5 +1,13 @@
 package gw2api
 
+// Item is an game item
+type Item struct {
+	ID         int         `json:"id"`
+	Name       string      `json:"name"`
+	Icon       string      `json:"icon"`
+	Attributes *Attributes `json:"attributes"`
+}
+
 // ItemStack is the data contained in requests that return stored items, e.g. bank or shared inventory
 type ItemStack struct {
 	ID        string `json:"id"`
