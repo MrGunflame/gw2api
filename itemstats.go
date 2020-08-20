efa50942fafa7ef3569fae051e74d7df218c204d
0xc0001a2040 <robert_0.1@gmx.de>
2020-08-20 22:30:43 +0200

    Add /v2/itemstats endpoint

--- itemstats.go
+++ itemstats.go
@@ -2,6 +2,17 @@ package gw2api
 
 // Itemstat contains the stattype and attributes of an item
 type Itemstat struct {
-	ID         int            `json:"id"`
-	Attributes map[string]int `json:"attributes"`
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Attributes []struct {
+		Attribute  string  `json:"attribute"`
+		Multiplier float64 `json:"multiplier"`
+		Value      int     `json:"value"`
+	} `json:"attributes"`
+}
+
+// GetItemStats returns a list of itemstats
+func (s *Session) GetItemStats(ids ...int) (itemstats []*Itemstat, err error) {
+	err = s.get(concatStrings("/v2/itemstats", genArgs(ids...)), &itemstats)
+	return
 }
