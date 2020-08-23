2c263b80a501ab3bcde3c1582641d74f83ed7e7a
0xc0001a2040 <robert_0.1@gmx.de>
2020-08-23 17:13:33 +0200

    Add GetLinkedWorlds

--- util/wvw.go
+++ util/wvw.go
@@ -4,6 +4,26 @@ import (
 	"gitlab.com/MrGunflame/gw2api"
 )
 
+// GetLinkedWorlds returns all linked worlds of a host world in the current match
+func GetLinkedWorlds(match *gw2api.WvWMatch, host int) []int {
+	var clr string
+	for k, v := range match.Worlds {
+		if v == host {
+			clr = k
+			break
+		}
+	}
+
+	var links []int
+	for _, w := range match.AllWorlds[clr] {
+		if w != host {
+			links = append(links, w)
+		}
+	}
+
+	return links
+}
+
 // IsLinkedWorld reports whether the entered world id is currently linked with the entered host id
 func IsLinkedWorld(match *gw2api.WvWMatch, host, link int) bool {
 	// Identify the host color
@@ -11,6 +31,7 @@ func IsLinkedWorld(match *gw2api.WvWMatch, host, link int) bool {
 	for k, v := range match.Worlds {
 		if v == host {
 			clr = k
+			break
 		}
 	}
 
