ba7d9e9d82204b581be67fa1723dd52840bb2229
0xc0001a2040 <robert_0.1@gmx.de>
2020-09-07 21:19:42 +0200

    Add ErrCannotUseAll

--- errors.go
+++ errors.go
@@ -9,6 +9,7 @@ var (
 	ErrNotFound           = errors.New("not found")
 	ErrNoAccessToken      = errors.New("No access token provided")
 	ErrInvalidAccessToken = errors.New("Invalid access token")
+	ErrCannotUseAll       = errors.New("unable to use 'all' keyword for this API")
 )
 
 // Error is an error returned from the gw2api
