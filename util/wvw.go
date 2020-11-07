c235f4c960f81edb6e4db8dda8373dd0cfd43b5c
0xc0001a2040 <robert_0.1@gmx.de>
2020-11-07 14:46:16 +0100

    Add PredictNextWvWMatches helper function

--- util/wvw.go
+++ util/wvw.go
@@ -1,6 +1,9 @@
 package util
 
 import (
+	"fmt"
+	"math"
+
 	"gitlab.com/MrGunflame/gw2api"
 )
 
@@ -42,4 +45,71 @@ func IsLinkedWorld(match *gw2api.WvWMatch, host, link int) bool {
 	}
 
 	return false
+}
+
+// PredictNextWvWMatches predicts the next weeks matchup based on the current point standings
+// The lowest tier cannot descend, the highest cannot ascend
+// The returned match only returns id and word data
+func PredictNextWvWMatches(matches []*gw2api.WvWMatch) []*gw2api.WvWMatch {
+	nextMatches := make([]*gw2api.WvWMatch, len(matches))
+	for i := range nextMatches {
+		nextMatches[i] = &gw2api.WvWMatch{
+			ID:     matches[i].ID,
+			Worlds: make(map[string]int),
+		}
+	}
+
+	inSlice := func(sl []string, s string) bool {
+		for _, v := range sl {
+			if v == s {
+				return true
+			}
+		}
+		return false
+	}
+
+	notIn := func(m *map[string]int, clr []string) string {
+		for k := range *m {
+			if !inSlice(clr, k) {
+				return k
+			}
+		}
+		return ""
+	}
+
+	for i, m := range matches {
+		fmt.Println(i)
+
+		var low, high string
+		lowVal, highVal := math.MaxInt64, 0
+		for k, v := range m.VictoryPoints {
+			if v < lowVal {
+				low = k
+				lowVal = v
+			}
+			if v > highVal {
+				high = k
+				highVal = v
+			}
+
+		}
+
+		// Top tier - cannot ascend further
+		if i == 0 {
+			nextMatches[i].Worlds["green"] = m.Worlds[high]
+		} else {
+			nextMatches[i-1].Worlds["red"] = m.Worlds[high]
+		}
+
+		// Bottom tier - cannot descend further
+		if i == len(matches)-1 {
+			nextMatches[i].Worlds["red"] = m.Worlds[low]
+		} else {
+			nextMatches[i+1].Worlds["green"] = m.Worlds[low]
+		}
+
+		nextMatches[i].Worlds["blue"] = m.Worlds[notIn(&m.Worlds, []string{low, high})]
+	}
+
+	return nextMatches
 }
---
+++ util/wvw_test.go
@@ -0,0 +1,108 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"gitlab.com/MrGunflame/gw2api"
+)
+
+func TestPredictNextWvWMatches(t *testing.T) {
+	cases := []struct {
+		given  []*gw2api.WvWMatch
+		expect []*gw2api.WvWMatch
+	}{
+		{
+			given: []*gw2api.WvWMatch{
+				{
+					ID: "2-1",
+					Worlds: map[string]int{
+						"red":   2013,
+						"blue":  2206,
+						"green": 2010,
+					},
+					AllWorlds: map[string][]int{
+						"red":   []int{2009, 2013},
+						"blue":  []int{2204, 2206},
+						"green": []int{2008, 2010},
+					},
+					VictoryPoints: map[string]int{
+						"red":   78,
+						"blue":  98,
+						"green": 109,
+					},
+				},
+				{
+					ID: "2-2",
+					Worlds: map[string]int{
+						"red":   2203,
+						"blue":  2301,
+						"green": 2202,
+					},
+					AllWorlds: map[string][]int{
+						"red":   []int{2006, 2203},
+						"blue":  []int{2301},
+						"green": []int{2207, 2202},
+					},
+					VictoryPoints: map[string]int{
+						"red":   86,
+						"blue":  88,
+						"green": 102,
+					},
+				},
+				{
+					ID: "2-3",
+					Worlds: map[string]int{
+						"red":   2014,
+						"blue":  2007,
+						"green": 2012,
+					},
+					AllWorlds: map[string][]int{
+						"red":   []int{2201, 2014},
+						"blue":  []int{2011, 2007},
+						"green": []int{2004, 2012},
+					},
+					VictoryPoints: map[string]int{
+						"red":   79,
+						"blue":  94,
+						"green": 103,
+					},
+				},
+			},
+			expect: []*gw2api.WvWMatch{
+				{
+					ID: "2-1",
+					Worlds: map[string]int{
+						"red":   2202,
+						"blue":  2206,
+						"green": 2010,
+					},
+				},
+				{
+					ID: "2-2",
+					Worlds: map[string]int{
+						"red":   2012,
+						"blue":  2301,
+						"green": 2013,
+					},
+				},
+				{
+					ID: "2-3",
+					Worlds: map[string]int{
+						"red":   2014,
+						"blue":  2007,
+						"green": 2203,
+					},
+				},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		result := PredictNextWvWMatches(c.given)
+		if !cmp.Equal(result, c.expect) {
+			t.Errorf("PredictNextWvWMatches failed: '%s'", cmp.Diff(result, c.expect))
+		}
+	}
+}
